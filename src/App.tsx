0b13ba4b5e087c3b55f42af82a8553c21920d782
H5Ryder <ryderharris@icloud.com>
2024-07-12 10:22:01 +0100

    Fix Error import React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './stopwatch.css';
 import Stopwatch from './components/Stopwatch';
 import LapDisplay from './components/LapDisplay';
